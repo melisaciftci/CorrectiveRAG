8c1f44ed1dc5a9d2a01eeef4ba901b2ca8fff294
Melisa Ciftci <ciftcimelisa03@gmail.com>
2026-01-17 19:26:40 +0300

    grading documents relevant or not

---
+++ graph/nodes/grade_documents.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict
+from graph.chains.retrieval_grader import retrieval_grader
+from graph.state import GraphState
+# Nodelarda açıklama yazmak önemli
+def grade_documents(state: GraphState) -> Dict[str, Any]:
+    """
+    Determine whether the retrieved documents are relevant to the question
+    If any document is not relevant, we will set a flag to run web search
+
+    Args:
+        state (dict): The current state of the graph
+
+    Returns:
+        state (dict): Filtered out irrevelant documents and updated web_search state
+    """
+
+    print("----CHECK DOCUMENT RELEVANT TO QUESTION")
+
+    question = state["question"]
+    documents = state["documents"]
+
+    filtered_docs=[]
+    web_search = False
+    for d in documents:
+        score = retrieval_grader.invoke(
+            {"question": question, "document": d.page_content}
+        )
+
+        grade = score.binary_score
+
+        if grade.lower() == "yes":
+            print("----GRADE: DOCUMENT RELEVANT")
+            filtered_docs.append(d)
+        else:
+            print("----GRADE: DOCUMENT NOT RELEVANT")
+            web_search = True
+            continue
+    return {"question": question, "documents": filtered_docs, "web_search": web_search}
